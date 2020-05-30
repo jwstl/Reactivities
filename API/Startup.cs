32ee901feb0814e7324a9c4f84878ee80b859d13
Mustang Sky <hello@mustangsky.com>
2020-05-30 19:14:49 -0400

    EF Initial Migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Persistance;
+using Persistence;
 
 namespace API
 {
@@ -27,13 +27,18 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddDbContext<DataContext>(opt =>
-                {
-                    opt.UseSqlite(Configuration.GetConnectionString("Default"));
-                });
-
             services.AddControllers();
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                //opt.UseLazyLoadingProxies();
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            ConfigureServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -2,13 +2,13 @@
 using Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistance
+namespace Persistence
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-            
+             
         }
 
         public DbSet<Value> Values {get;set;}
