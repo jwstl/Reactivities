37d6f4a44301716b378879136af96792622a0a47
Mustang Sky <hello@mustangsky.com>
2020-06-10 17:08:48 -0400

    Updated App

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { cars } from "./demo";
-import CarItem from "./CarItem";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+// import { cars } from "./demo";
+// import CarItem from "./CarItem";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <ul>
-      </ul>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul></ul>
+      </div>
+    )
+  }
+}
 
-export default App;
+export default App
